625922ffc47ea8a0a6425b4b45d296660dd2ebaa
Samuel <kungusamuel64@gmail.com>
2022-04-02 23:04:41 +0300

    Final Product

---
+++ src/constants/constants.js
@@ -0,0 +1,46 @@
+export const projects = [
+  {
+    title: 'Send It',
+    description: "A simple courier delivery app that allows users deliver parcels to different destinations.",
+      image: '/images/sendit.png',
+      tags: ['Microsoft SQL Server', 'Express', 'React', 'Node'],
+    source: 'https://github.com/samuelkungu/send-it',
+    visit: 'https://github.com/samuelkungu/send-it',
+    id: 0,
+  },
+  // {
+  //   title: 'Buy It',
+  //   description:"This is a native android application that allows users deliver parcels to different destinations",
+  //   image: '/images/2.png',
+  //   tags: ['Java', 'Firebase'],
+  //   source: 'https://google.com',
+  //   visit: 'https://google.com',
+  //   id: 1,
+  // },
+  {
+    title: 'Todo App',
+    description: "This is a simple todo app to track your tasks. It is flexible enough to adapt to most workflows but not so complicated as to overwhelm. The main interface is clean and friendly, adding tasks is quick, but there's a lot of flexibility below the surface.",
+      image: '/images/todo.png',
+      tags: ['React'],
+    source: 'https://github.com/samuelkungu/todo-app',
+    visit: 'https://github.com/samuelkungu/todo-app',
+    id: 2,
+  },
+  // {
+  //   title: 'Unichat',
+  //   description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+  //   image: '/images/4.jpg',
+  //   tags: ['React', 'ChatEngine', 'Firebase'],
+  //   source: 'https://google.com',
+  //   visit: 'https://google.com',
+  //   id: 3,
+  // },
+];
+
+export const TimeLineData = [
+  { year: 2019, text: 'Bsc. Computer Science Dedan Kimathi University of Technology', },
+  { year: 2021, text: 'Computer Science Internal Attachment', },
+  { year: 2022, text: 'Web Development Training at The Jitu.com Cohort 3', },
+  { year: 2022, text: 'Web Development Featured Project at the Nyeri Golf Club ', },
+  { year: 2022, text: 'Started my own platform', },
+];
